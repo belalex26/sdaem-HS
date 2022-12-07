36405bd635cc10077aecedd81ae9e58caa2216fb
Belalex26 <alex.belova26@gmail.com>
2022-12-07 23:25:47 +0300

    валидация в авторизации

--- src/components/auth-form/auth-form.tsx
+++ src/components/auth-form/auth-form.tsx
@@ -1,11 +1,23 @@
+import { useForm } from "react-hook-form";
+
+type FormData = {
+    login: string,
+    password: string
+  };
+
 function AuthForm() {
+  const { register, setValue, handleSubmit, formState: { errors } } = useForm<FormData>();
+  const onSubmit = handleSubmit(data => console.log(data));
+
   return (
     <form className="auth-form">
         <label className="auth-form__label">
-            <input className="auth-form__input" type="text" name="name" id="name" />
+            <input className="auth-form__input" type="text" {...register("login", { required: true, minLength: 3 })}/>
+            {errors.login && "Заполните поле"}
         </label>
         <label className="auth-form__label">
-            <input className="auth-form__input" type="password" name="password" id="password" />
+            <input className="auth-form__input" type="password" {...register("login", { required: true})}/>
+            {errors.password && "Заполните поле"}
         </label>
         <button className="auth-form__btn" type="submit">Войти</button>
         <label className="auth-form__label">
